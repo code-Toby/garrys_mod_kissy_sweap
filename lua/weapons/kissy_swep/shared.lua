7fa07813c445a22c1965e0763d99e83d41f20053
BuildTools <unconfigured@null.spigotmc.org>
2023-07-22 06:33:20 -0400

    added modeling code

--- lua/weapons/kissy_swep/shared.lua
+++ lua/weapons/kissy_swep/shared.lua
@@ -1,10 +1,10 @@
-if SERVER then
-    AddCSLuaFile("shared.lua")
+AddCSLuaFile()
+include('swepModelerCode.lua')
 
-    SWEP.HoldType = "normal"
-    SWEP.Weight = 5
-	SWEP.AutoSwitchTo = false
-	SWEP.AutoSwitchFrom = false
+if SERVER then
+    SWEP.Weight             = 5
+	SWEP.AutoSwitchTo       = false
+	SWEP.AutoSwitchFrom     = false
 end
 
 if CLIENT then
@@ -14,31 +14,38 @@ if CLIENT then
 	SWEP.SlotPos			= 10
 
     SWEP.DrawAmmo = false
-    SWEP.DrawCrosshair = false
+    SWEP.DrawCrosshair = true
 end
 
-SWEP.Spawnable = true
-SWEP.AdminSpawnable = false
+SWEP.Spawnable              = true
+SWEP.AdminSpawnable         = false
 
-
-
+SWEP.HoldType = "slam"
+SWEP.ViewModelFOV = 70
+SWEP.ViewModelFlip = false
+SWEP.UseHands = false
 SWEP.ViewModel = ""
 SWEP.WorldModel = "models/balloons/balloon_classicheart.mdl"
-
-SWEP.ShowWorldModel = false
 SWEP.ShowViewModel = false
+SWEP.ShowWorldModel = false
+SWEP.ViewModelBoneMods = {}
 
-
-
-SWEP.Primary.ClipSize = -1
-SWEP.Primary.DefaultClip = -1
-SWEP.Primary.Automatic = false
-SWEP.Primary.Ammo = "none"
+SWEP.Primary.ClipSize       = -1
+SWEP.Primary.DefaultClip    = -1
+SWEP.Primary.Automatic      = false
+SWEP.Primary.Ammo           = "none"
  
-SWEP.Secondary.ClipSize = -1
-SWEP.Secondary.DefaultClip = -1
-SWEP.Secondary.Automatic = false
-SWEP.Secondary.Ammo = "none"
+SWEP.Secondary.ClipSize     = -1
+SWEP.Secondary.DefaultClip  = -1
+SWEP.Secondary.Automatic    = false
+SWEP.Secondary.Ammo         = "none"
+
+
+SWEP.WElements = {
+	["Heart++"] = { type = "Model", model = "models/balloons/balloon_classicheart.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.635, 11.947, -3.636), angle = Angle(47.922, -66.624, 180), size = Vector(0.172, 0.172, 0.172), color = Color(255, 0, 0, 255), surpresslightning = true, material = "models/debug/debugwhite", skin = 0, bodygroup = {} },
+	["Heart"] = { type = "Model", model = "models/balloons/balloon_classicheart.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.635, 3.635, 1.557), angle = Angle(22.208, -66.624, 180), size = Vector(0.432, 0.432, 0.432), color = Color(255, 83, 128, 255), surpresslightning = true, material = "models/debug/debugwhite", skin = 0, bodygroup = {} },
+	["Heart+"] = { type = "Model", model = "models/balloons/balloon_classicheart.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.635, -0.519, -3.636), angle = Angle(-17.532, -66.624, 180), size = Vector(0.172, 0.172, 0.172), color = Color(0, 255, 255, 255), surpresslightning = true, material = "models/debug/debugwhite", skin = 0, bodygroup = {} }
+}
 
 function SWEP:PrimaryAttack()
 
